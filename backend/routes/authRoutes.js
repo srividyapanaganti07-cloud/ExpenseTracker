f7c8521ef247bedd3949a8ed7785da5d7bf12490
Srividya <srividyapanganti07@gmail.com>
2026-09-19 13:24:00 +0530

    Fix User model import case

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
 const crypto = require("crypto");
-const User = require("../models/User");
+const User = require("../models/user");
 const sendResetEmail = require("../utils/emailService");
 
 const router = express.Router();
